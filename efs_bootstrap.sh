b8193153285f6c4936c7890cc8ed6224d8c6cd11
keshavkummari <keshav.kummari@gmail.com>
2019-08-22 22:55:27 +0530

    efs

---
+++ efs_bootstrap.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+yum install wget* curl* vim* unzip* -y
+yum update -y
+yum install http* --skip-broken -y
+mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-af70c14d.efs.us-east-1.amazonaws.com:/ /var/www/html/
+cd /opt/
+wget https://www.free-css.com/assets/files/free-css-templates/download/page231/bizexpress-v1.0.1.zip
+unzip *.zip
+cd Biz*
+mv * /var/www/html/
+service httpd start
